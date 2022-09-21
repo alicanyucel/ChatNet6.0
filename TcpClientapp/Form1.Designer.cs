bc5fd0f05ef1ba47807d2cb43f694cfc5dd264c4
ali can yücel <yucelalican@hotmail.com>
2022-09-21 20:36:11 +0300

    client tarafı kodlandı

--- TcpClientapp/Form1.Designer.cs
+++ TcpClientapp/Form1.Designer.cs
@@ -28,12 +28,104 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtIp = new System.Windows.Forms.TextBox();
+            this.txtBaslik = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.TxtMesaj = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(62, 65);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Server";
+            // 
+            // txtIp
+            // 
+            this.txtIp.Location = new System.Drawing.Point(131, 57);
+            this.txtIp.Name = "txtIp";
+            this.txtIp.Size = new System.Drawing.Size(202, 23);
+            this.txtIp.TabIndex = 1;
+            this.txtIp.Text = "10.10.105.249:139";
+            // 
+            // txtBaslik
+            // 
+            this.txtBaslik.Location = new System.Drawing.Point(131, 100);
+            this.txtBaslik.Multiline = true;
+            this.txtBaslik.Name = "txtBaslik";
+            this.txtBaslik.Size = new System.Drawing.Size(202, 202);
+            this.txtBaslik.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(62, 338);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Message";
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(364, 334);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 4;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(460, 335);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(75, 23);
+            this.btnConnect.TabIndex = 5;
+            this.btnConnect.Text = "baglan";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // TxtMesaj
+            // 
+            this.TxtMesaj.Location = new System.Drawing.Point(121, 335);
+            this.TxtMesaj.Name = "TxtMesaj";
+            this.TxtMesaj.Size = new System.Drawing.Size(227, 23);
+            this.TxtMesaj.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.TxtMesaj);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtBaslik);
+            this.Controls.Add(this.txtIp);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private Label label1;
+        private TextBox txtIp;
+        private TextBox txtBaslik;
+        private Label label2;
+        private Button btnSend;
+        private Button btnConnect;
+        private TextBox TxtMesaj;
     }
 }
--- TcpClientapp/Form1.cs
+++ TcpClientapp/Form1.cs
@@ -1,3 +1,6 @@
+using SuperSimpleTcp;
+using System.Text;
+
 namespace TcpClientapp
 {
     public partial class Form1 : Form
@@ -6,7 +9,61 @@ namespace TcpClientapp
         {
             InitializeComponent();
         }
+        SimpleTcpClient client;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            client = new(txtIp.Text);
+            client.Events.Connected += Events_Connected;
+            client.Events.DataReceived += Events_DataReceived;
+            client.Events.Disconnected += Events_Disconnected;
+            btnSend.Enabled = false;
 
+        }
 
+        private void Events_Disconnected(object? sender, ConnectionEventArgs e)
+        {
+            txtBaslik.Text += $"Server.diconnected.{Environment.NewLine}";
+        }
+
+        private void Events_DataReceived(object? sender, DataReceivedEventArgs e)
+        {
+
+            txtBaslik.Text += $"Server:{Encoding.UTF8.GetString(e.Data)}{Environment.NewLine}";
+        }
+
+        private void Events_Connected(object? sender, ConnectionEventArgs e)
+        {
+            txtBaslik.Text += $"Server connected. {Environment.NewLine}";
+
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            if (client.IsConnected)
+            {
+                if (!string.IsNullOrEmpty(TxtMesaj.Text))
+                {
+                    client.Send(TxtMesaj.Text);
+                    txtBaslik.Text += $"ME: {TxtMesaj.Text}{Environment.NewLine}";
+                    TxtMesaj.Text = String.Empty;
+
+                }
+            }
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                client.Connect();
+                btnConnect.Enabled = false;
+                btnSend.Enabled = true;
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
     }
 }
